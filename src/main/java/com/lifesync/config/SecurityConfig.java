c93b5cc19cc8728bd6157f8b709a73c6ff12b44d
akhil-2kb2 <rudraaraju@gmail.com>
2025-05-01 17:06:38 +0530

    updated congig

--- src/main/java/com/lifesync/config/SecurityConfig.java
+++ src/main/java/com/lifesync/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
             )
             .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+            .userDetailsService(userDetailsService)
             .build();
     }
     
